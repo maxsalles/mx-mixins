c0ace1edd3be59923c678297affd3e3aced2c9a7
Max Salles <herond12@gmail.com>
2020-10-24 18:07:32 -0300

    revert(github-actions): undo intentional failure in the tests

--- test/apply.spec.js
+++ test/apply.spec.js
@@ -21,7 +21,7 @@ describe('apply', () => {
       expect(mixin).toBeCalledWith(Class)
       expect(Object.getOwnPropertyDescriptor(mixed, APPLIED_MIXIN_PROPERTY)).toEqual({
         value: mixin,
-        writable: true,
+        writable: false,
         configurable: false,
         enumerable: false
       })
